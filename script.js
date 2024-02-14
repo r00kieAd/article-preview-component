1fe27a36fe703b2939fc6586e9b2409f80409aa2
Adhyatma Dwivedi <113773621+r00kieAd@users.noreply.github.com>
2024-02-14 21:55:57 +0530

    added mobile view and logic

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 $('.share-button').click(
-    function() {
-        $('#hover-div').fadeToggle();
+    function () {
+        if (screen.width <= 900) {
+            $('#profile-info').toggleClass('hide-profile');
+            $('#mobile-hover-div-container').toggle();
+        } else {
+            $('#hover-div').fadeToggle();
+        }
     }
 )
 
 $('.share-button').mouseenter(
-    function() {
+    function () {
         $(this).css('background-color', 'hsl(217, 19%, 35%)');
         $('path').attr('fill', '#ffffff');
     }
 )
 
 $('.share-button').mouseleave(
-    function() {
+    function () {
         $(this).css('background-color', 'hsl(210, 46%, 95%)')
         $('path').attr('fill', '#6E8098');
     }
